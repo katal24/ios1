3ed710d4059e9578259e8fefe764fba7318f302e
Użytkownik Gość <Guest@imac-07.local>
2017-12-15 11:17:38 +0100

    Initial Commit

---
+++ talaga1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  talaga1
+//
+//  Created by Użytkownik Gość on 15.12.2017.
+//  Copyright © 2017 talaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
